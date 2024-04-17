dc0fee72946ad812c0a8deb99f87254dff22edf7
Kleber777 <klebermirandalima@gmail.com>
2024-04-16 21:43:48 -0300

    Arrumando o ambiente de desenvolvimento

--- src/app/dashboard/layout.jsx
+++ src/app/dashboard/layout.jsx
@@ -3,7 +3,7 @@ import { MenuBar } from "./components/menu";
 
 export default function DashboardLayout({children}){
     return(
-        <div className="min-h-screen bg-[#FCFAFA]">
+        <div className="min-h-screen bg-[#FCFAFA] text-white">
             <div className="p-12 bg-[#07510A]">
                 <MenuBar />
             </div>
